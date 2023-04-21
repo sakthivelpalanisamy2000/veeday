5d2fafb055248bfbb89ca8c5144a0fb781b0a599
sakthivel palanismay <sakthivelcse21@gmail.com>
2023-04-21 18:49:30 +0530

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Company
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
